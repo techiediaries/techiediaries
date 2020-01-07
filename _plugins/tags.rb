93c3a0ef20920e2343f0aaa34df836d8083a9d3b
techiediaries <techiediaries9@gmail.com>
2020-01-07 03:49:09 +0100

    tags plugin

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -15,18 +15,35 @@ module Jekyll
         self.data['title'] = "#{tag_title_prefix}#{tag}"
       end
     end
+
+    class SpecificTagPage < Page
+      def initialize(site, base, dir, tag)
+        @site = site
+        @base = base
+        @dir = dir
+        @name = 'index.html'
   
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), '#{tag}.html')
+        self.data['tag'] = tag
+  
+        tag_title_prefix = site.config['tag_title_prefix'] || 'Tag: '
+        self.data['title'] = "#{tag_title_prefix}#{tag}"
+      end
+    end    
+
     class TagPageGenerator < Generator
       safe true
   
       def generate(site)
-        if site.layouts.key? 'tag-index'
-          dir = site.config['tag_dir'] || 'topics'
-          site.tags.keys.each do |tag|
+        site.tags.keys.each do |tag|
+          if site.layouts.key? tag
+            site.pages << SpecificTagPage.new(site, site.source, File.join(dir, tag), tag)
+          else
             site.pages << TagPage.new(site, site.source, File.join(dir, tag), tag)
           end
+
         end
       end
     end
-  
   end
